fb87db33bb0c5a759b2c79cfc0e95f870083028e
Ankit Varshney <varshneyankit011@gmail.com>
2024-01-01 12:22:30 +0530

    header completed and added the images on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,50 @@
+/* eslint-disable @next/next/no-img-element */
+import { trending_data } from "@/data/trending";
+
 export default function Home() {
   return (
-    <main className="">
-     {/* <h1>Booking.com clone</h1> */}
+    <main className="bg-[#013B94]">
+      <section className="max-w-7xl mx-auto p-6">
+        <h2 className="font-bold text-5xl text-white">Find your Next Stay</h2>
+        <h3 className="text-white py-5 text-xl">
+          Search low prices on hotels, homes and much more...
+        </h3>
+      </section>
+
+      <section className="-mb-14 px-2 mt-0 m-4 lg:px-4">
+        {/* SearchForm */}
+      </section>
+
+      <section className="max-w-7xl mx-auto p-6 mt-10 bg-white rounded-t-lg">
+        <div className="pt-5">
+          <h3 className="text-xl font-bold">Trending Destinations</h3>
+          <p className="font-light">
+            Most popular choices for travellers from around the world
+          </p>
+        </div>
+
+        <div className="flex space-x-4 py-5 overflow-x-scroll">
+          {trending_data.map((item) => (
+            <div key={item.id} className="space-y-1 shrink-0 cursor-pointer">
+              <img
+                className="w-80 h-72 object-cover rounded-lg pb-2"
+                key={item.id}
+                src={item.src}
+                alt=""
+              />
+              <p className="font-bold">
+                {item.title}
+              </p>
+              <p className="">
+                {item.location}
+              </p>
+              <p className="font-light text-sm">
+                {item.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
     </main>
-  )
+  );
 }
